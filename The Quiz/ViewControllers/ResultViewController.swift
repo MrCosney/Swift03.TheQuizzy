ac9fac4092d42902c63af00cfef23815ad511d52
Nikita Kuzmin <Dr.Cappo@gmail.com>
2021-03-23 22:01:42 +0300

    Fix Folders

---
+++ The Quiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ResultViewController.swift
+//  The Quiz
+//
+//  Created by Nick on 23.03.2021.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.hidesBackButton = true
+        // Do any additional setup after loading the view.
+    }
+
+}
