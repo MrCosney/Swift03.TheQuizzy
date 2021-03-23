53d4553fd72b6ad62cda8e5953445f4824aac7e7
Nikita Kuzmin <Dr.Cappo@gmail.com>
2021-03-23 21:56:29 +0300

    Finished 1st screen, need fix the font in mainLabel

---
+++ App/AudioPlayer.swift
@@ -0,0 +1,37 @@
+//
+//  AudioPlayer.swift
+//  The Quiz
+//
+//  Created by Nick on 23.03.2021.
+//
+
+import AVFoundation
+
+
+/// Class for control the Background Music Status
+class Music {
+    static let sharedInstance = Music()
+    private var player: AVAudioPlayer?
+    
+    //Turn On the Background Music
+    func play() {
+        guard let url = Bundle.main.url(forResource: "musicTheme", withExtension: "mp3") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.soloAmbient)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            player?.numberOfLoops = -1
+            guard let player = player else { return }
+            player.volume = 0.1
+            player.play()
+            
+        } catch { print("Background Music is not found") }
+    }
+    
+    //Stop the Background Music
+    func stop() {
+        player?.stop()
+    }
+}
